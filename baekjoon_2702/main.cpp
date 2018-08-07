31da648243cfdd24fcb1d4d3abd7a89201928a6d
WaniFi <skelpd24@gmail.com>
2018-08-07 21:23:53 +0900

    2702 문제 풀이 완료

--- baekjoon_2702/main.cpp
+++ baekjoon_2702/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
+int main() {
+	int a, b, m, n, t, tmp;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+		m = a; n = b;
+		while (m%n != 0) {
+			tmp = n;
+			n = m % n;
+			m = tmp;
+		}
+		cout << (a*b) / n << " " << n << endl;
+	}
+	return 0;
 }
